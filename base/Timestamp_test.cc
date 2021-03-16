d6ca453987790271669fc02ce9d3653e3dba14ce
wangml <wangmaolin1126@163.com>
2021-03-16 20:18:54 +0800

    Create Timestamp_test.cc

---
+++ base/Timestamp_test.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Timestamp.h"
+#include <vector>
+
+int main()
+{
+  Timestamp time1(Timestamp::now());
+  std::cout << time1.toString() << std::endl;
+  std::cout << time1.toFormattedString(true) << std::endl;
+  std::cout << time1.toFormattedString(false) << std::endl;
+  
+  // 创建1000000个Timestamp个对象所需要的时间
+  const int kNumber = 1000*1000;
+  std::vector<Timestamp> stamps;
+  stamps.reserve(kNumber);
+
+  for (int i = 0; i < kNumber; i++) {
+    stamps.push_back(Timestamp::now());
+  }
+
+  std::cout << stamps.front().toFormattedString(false) << std::endl;
+  std::cout << stamps.back().toFormattedString(false) << std::endl;
+  std::cout << timeDifference(stamps.back(), stamps.front()) << std::endl;
+
+
+
+  return 0;
+}
+// 编译命令
+// g++ Timestamp.cc Timestamp_test.cc -o Timestamp_test
