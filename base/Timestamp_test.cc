0f5abb45ce67584e2cfb2427eb6215218a3caa27
wangml <wangmaolin1126@163.com>
2021-03-17 10:10:18 +0800

    Update Timestamp_test.cc

--- base/Timestamp_test.cc
+++ base/Timestamp_test.cc
@@ -4,18 +4,18 @@
 
 int main()
 {
-  Timestamp time1(Timestamp::now());
+  muduo::Timestamp time1(muduo::Timestamp::now());
   std::cout << time1.toString() << std::endl;
   std::cout << time1.toFormattedString(true) << std::endl;
   std::cout << time1.toFormattedString(false) << std::endl;
   
   // 创建1000000个Timestamp个对象所需要的时间
   const int kNumber = 1000*1000;
-  std::vector<Timestamp> stamps;
+  std::vector<muduo::Timestamp> stamps;
   stamps.reserve(kNumber);
 
   for (int i = 0; i < kNumber; i++) {
-    stamps.push_back(Timestamp::now());
+    stamps.push_back(muduo::Timestamp::now());
   }
 
   std::cout << stamps.front().toFormattedString(false) << std::endl;
