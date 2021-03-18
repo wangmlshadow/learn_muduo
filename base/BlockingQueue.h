c9d84f553f21c7d842a5e56068ca1260a8ab689e
wangml <wangmaolin1126@163.com>
2021-03-18 09:56:42 +0800

    Create BlockingQueue.h

---
+++ base/BlockingQueue.h
@@ -0,0 +1,74 @@
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#ifndef MUDUO_BASE_BLOCKINGQUEUE_H
+#define MUDUO_BASE_BLOCKINGQUEUE_H
+
+//#include "muduo/base/Condition.h"
+#include "Condition.h"
+//#include "muduo/base/Mutex.h"
+#include "Mutex.h"
+
+#include <deque>
+#include <assert.h>
+
+namespace muduo
+{
+
+template<typename T>
+class BlockingQueue : noncopyable   // 无界缓冲区
+{
+ public:
+  BlockingQueue()
+    : mutex_(),
+      notEmpty_(mutex_),
+      queue_()
+  {
+  }
+
+  void put(const T& x) // 向队列添加元素
+  {
+    MutexLockGuard lock(mutex_); // 锁
+    queue_.push_back(x);
+    notEmpty_.notify(); // wait morphing saves us   // 添加完元素后就非空了 发出通知
+    // http://www.domaigne.com/blog/computing/condvars-signal-with-mutex-locked-or-not/
+  }
+
+  void put(T&& x)
+  {
+    MutexLockGuard lock(mutex_);
+    queue_.push_back(std::move(x));
+    notEmpty_.notify();
+  }
+
+  T take() // 取一个元素出来
+  {
+    MutexLockGuard lock(mutex_);
+    // always use a while-loop, due to spurious wakeup
+    while (queue_.empty()) // 队列非空才能取
+    {
+      notEmpty_.wait();
+    }
+    assert(!queue_.empty());
+    T front(std::move(queue_.front()));
+    queue_.pop_front();
+    return front;
+  }
+
+  size_t size() const // 队列大小
+  {
+    MutexLockGuard lock(mutex_);
+    return queue_.size();
+  }
+
+ private:
+  mutable MutexLock mutex_;
+  Condition         notEmpty_ GUARDED_BY(mutex_);
+  std::deque<T>     queue_ GUARDED_BY(mutex_);
+};
+
+}  // namespace muduo
+
+#endif  // MUDUO_BASE_BLOCKINGQUEUE_H
