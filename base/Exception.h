86c773e6daa89673de4ecee49bc6da057c40b4f4
wangml <wangmaolin1126@163.com>
2021-03-18 10:06:51 +0800

    Create Exception.h

---
+++ base/Exception.h
@@ -0,0 +1,42 @@
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#ifndef MUDUO_BASE_EXCEPTION_H
+#define MUDUO_BASE_EXCEPTION_H
+
+//#include "muduo/base/Types.h"
+#include "Types.h"
+
+#include <exception>
+
+namespace muduo
+{
+
+class Exception : public std::exception // 异常类的实现
+{
+ public:
+  Exception(string what);
+  ~Exception() noexcept override = default;
+
+  // default copy-ctor and operator= are okay.
+
+  const char* what() const noexcept override // 异常信息
+  {
+    return message_.c_str();
+  }
+
+  const char* stackTrace() const noexcept // 异常栈信息
+  {
+    return stack_.c_str();
+  }
+
+ private:
+  string message_;
+  string stack_;
+};
+
+}  // namespace muduo
+
+#endif  // MUDUO_BASE_EXCEPTION_H
