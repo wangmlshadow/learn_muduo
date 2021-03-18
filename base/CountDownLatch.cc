52bfbe20389f031d34a3118f05785adbfe437fc7
wangml <wangmaolin1126@163.com>
2021-03-18 10:02:42 +0800

    Create CountDownLatch.cc

---
+++ base/CountDownLatch.cc
@@ -0,0 +1,43 @@
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+//#include "muduo/base/CountDownLatch.h"
+#include "CountDownLatch.h"
+
+
+using namespace muduo;
+
+CountDownLatch::CountDownLatch(int count)
+  : mutex_(),
+    condition_(mutex_),
+    count_(count)
+{
+}
+
+void CountDownLatch::wait()// 等待
+{
+  MutexLockGuard lock(mutex_);
+  while (count_ > 0)
+  {
+    condition_.wait();
+  }
+}
+
+void CountDownLatch::countDown()
+{
+  MutexLockGuard lock(mutex_);
+  --count_;
+  if (count_ == 0)// 计数器减为0发起通知
+  {
+    condition_.notifyAll();
+  }
+}
+
+int CountDownLatch::getCount() const
+{
+  MutexLockGuard lock(mutex_);
+  return count_;
+}
+
